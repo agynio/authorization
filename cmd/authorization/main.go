5b7f39fd03070ae870bbcfdedee1ea5274a3b330
casey-brooks <casey.brooks@agyn.io>
2026-03-14 17:32:47 +0000

    feat: add authorization service, Helm chart, CI, and Terraform module
    * feat: add authorization service
    
    * fix: require OpenFGA model id
    
    * fix: address review feedback

---
+++ cmd/authorization/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	authorizationv1 "github.com/agynio/authorization/gen/go/agynio/api/authorization/v1"
+	"google.golang.org/grpc"
+
+	"github.com/agynio/authorization/internal/config"
+	"github.com/agynio/authorization/internal/server"
+	openfgaclient "github.com/openfga/go-sdk/client"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatalf("authorization: %v", err)
+	}
+}
+
+func run() error {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	cfg, err := config.FromEnv()
+	if err != nil {
+		return err
+	}
+
+	fgaClient, err := openfgaclient.NewSdkClient(&openfgaclient.ClientConfiguration{
+		ApiUrl: cfg.OpenFGAAPIURL,
+	})
+	if err != nil {
+		return fmt.Errorf("create OpenFGA client: %w", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	authorizationv1.RegisterAuthorizationServiceServer(
+		grpcServer,
+		server.New(fgaClient, cfg.OpenFGAStoreID, cfg.OpenFGAModelID),
+	)
+
+	lis, err := net.Listen("tcp", cfg.GRPCAddress)
+	if err != nil {
+		return fmt.Errorf("listen on %s: %w", cfg.GRPCAddress, err)
+	}
+
+	go func() {
+		<-ctx.Done()
+		grpcServer.GracefulStop()
+	}()
+
+	log.Printf("AuthorizationService listening on %s", cfg.GRPCAddress)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		if errors.Is(err, grpc.ErrServerStopped) {
+			return nil
+		}
+		return fmt.Errorf("serve: %w", err)
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/agynio/authorization
+
+go 1.24.0
+
+require (
+	github.com/openfga/go-sdk v0.7.5
+	google.golang.org/grpc v1.79.1
+	google.golang.org/protobuf v1.36.11
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.opentelemetry.io/otel v1.40.0 // indirect
+	go.opentelemetry.io/otel/metric v1.40.0 // indirect
+	go.opentelemetry.io/otel/trace v1.40.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/net v0.48.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
+)
